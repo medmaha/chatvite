8a2789f3a4943ec2076df687f55d6afafccd0dd2
Mahamed Toure <104234458+medmaha@users.noreply.github.com>
2024-05-17 21:44:10 +0000

    Update cron.js

--- pages/api/cron.js
+++ pages/api/cron.js
@@ -1,9 +1,11 @@
 import axios from "axios"
 
 export default async function handler(req, res) {
-  try{ 
-    await axios.get("https://websocket-chatvite.glitch.me/health/")
-  }
-  catch(error){}
-  res.status(204).end();
+  return new Promise(resolve=>{
+    try{ 
+      axios.get("https://websocket-chatvite.glitch.me/health/").then(resolve).catch(_){}
+    }
+    catch(error){}
+    res.status(204).end();
+  })
 }
