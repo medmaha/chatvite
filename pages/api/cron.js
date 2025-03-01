35f741f222334b9438e3e86b1ec25ab577019ad7
medmaha <toure295@outlook.com>
2025-03-01 16:20:40 +0000

    Update cron.js error handling and bump axios and other dependencies

--- pages/api/cron.js
+++ pages/api/cron.js
@@ -3,7 +3,7 @@ import axios from "axios"
 export default async function handler(req, res) {
   return new Promise(resolve=>{
     try{ 
-      axios.get("https://websocket-chatvite.glitch.me/health/").then(resolve).catch(_){}
+      axios.get("https://websocket-chatvite.glitch.me/health/").then(resolve).catch(_=>{})
     }
     catch(error){}
     res.status(204).end();
